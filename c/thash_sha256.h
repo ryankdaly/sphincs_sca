8567e5c1a287ca21282d966dd703bf38fe76e09a
PhoebeMensah <phoebemensah@Phoebes-MacBook-Pro.local>
2026-04-13 00:24:20 -0400

    initial commit for c implementation

--- c/thash_sha256.h
+++
@@ -1,9 +0,0 @@
-#ifndef THASH_SHA256_H
-#define THASH_SHA256_H
-
-#include <stdint.h>
-
-void thash(uint8_t *out, const uint8_t *in, unsigned int inblocks,
-           const uint8_t *pub_seed, const uint8_t *addr);
-
-#endif
